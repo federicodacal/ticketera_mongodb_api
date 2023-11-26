4a8473c7a7ab72aa5fd63cff71a46f05fce6d262
Federico Dacal <federicodacal@gmail.com>
2023-11-26 18:25:46 -0300

    vercel

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const client = new MongoClient(process.env.URI);
 app.get('/api/tickets', async (req, res) => {
     try {
         
-        const database = client.db('sample_airbnb');
-        const movies = database.collection('listingsAndReviews');
+        const db = client.db('ticketera');
+        const tickets = db.collection('tickets');
         
-        const data = await movies.find().limit(2).toArray();
+        const data = await tickets.find().toArray();
 
         return res.status(200).json({
             data
@@ -23,9 +23,6 @@ app.get('/api/tickets', async (req, res) => {
         return res.status(404).json({
             msg: err.message
         })
-    }
-    finally {
-        await client.close()
     }
 })
 
