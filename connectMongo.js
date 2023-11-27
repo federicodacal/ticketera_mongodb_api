7873e5df9a1c4438c1cf2057fb46c44919b7c4fb
Federico Dacal <federicodacal@gmail.com>
2023-11-27 15:47:47 -0300

    a ver

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected');
     }
     catch (err) {
