fab149f27051a56e601e9ca6e5c6b85214cb9818
Federico Dacal <federicodacal@gmail.com>
2023-12-01 15:50:35 -0300

    operadores gt, lt y lte

--- index.js
+++ index.js
@@ -87,6 +87,64 @@ app.get('/api/tickets_no_resueltos', async (req, res) => {
     }
 })
 
+// Operador $gt -> Usuarios con plan mayor a $900
+app.get('/api/plan_mayor_a_900', async (req, res) => {
+    try {
+        
+        const users = await Usuario.find({
+            "plan.precio": { $gt: 900 }
+        });
+
+        return res.status(200).json({
+            users
+        })
+    }
+    catch(err) {
+        return res.status(404).json({
+            msg: err.message
+        })
+    }
+})
+
+// Operador $lt -> Usuarios con plan menor a $900
+app.get('/api/plan_menor_a_900', async (req, res) => {
+    try {
+        
+        const users = await Usuario.find({
+            "plan.precio": { $lt: 900 }
+        });
+
+        return res.status(200).json({
+            users
+        })
+    }
+    catch(err) {
+        return res.status(404).json({
+            msg: err.message
+        })
+    }
+})
+
+// Operador $lt -> Usuarios con plan menor o igual a $550
+app.get('/api/plan_lte_550', async (req, res) => {
+    try {
+        
+        const users = await Usuario.find({
+            "plan.precio": { $lt: 550 }
+        });
+
+        return res.status(200).json({
+            users
+        })
+    }
+    catch(err) {
+        return res.status(404).json({
+            msg: err.message
+        })
+    }
+})
+
+
 
 app.listen(8080, () => {
     console.log('Server running on port 8080');
